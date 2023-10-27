7c97f993fc7f0fbde6d1dba2434b4a4f72b31c51
Nataliia Kudria <nataliia_kudria@epam.com>
2023-10-27 20:14:12 +0300

    refactored parameterized test scenario, refactored existing code

--- src/main/java/steps/ModalSteps.java
+++ src/main/java/steps/ModalSteps.java
@@ -1,16 +1,18 @@
 package steps;
 
+import static utils.StringUtils.getRandomStringFromFile;
+
 import org.jbehave.core.annotations.When;
 import org.openqa.selenium.WebElement;
 import utils.CustomLogger;
-import utils.FileHelper;
-import java.util.Random;
 
 public class ModalSteps extends BaseSteps {
 
+    private static final String FILE_NAME = "filterNaming";
+
     @When("On the 'ADD FILTER' modal, user fills in the name input field with random value and saves it to DataHolder")
     public void setFilterName() {
-        String filterName = getRandomStringFromFile("filterNaming");
+        String filterName = getRandomStringFromFile(FILE_NAME, 7);
         WebElement input = filterModal().getNameInputOnModal();
         clearAndThenFillInputField(input, filterName);
         unFocus();
@@ -25,20 +27,5 @@ public class ModalSteps extends BaseSteps {
 
     private void unFocus() {
         filterModal().getModalTitle().click();
-    }
-
-    private String getRandomStringFromFile(String file) {
-        String initialText = deleteNonAlphabeticChars(new FileHelper().readFile(file));
-        StringBuilder builderForString = new StringBuilder();
-        Random random = new Random();
-        while (builderForString.length() < 7) {
-            int index = (int) (random.nextFloat() * initialText.length());
-            builderForString.append(initialText.charAt(index));
-        }
-        return builderForString.toString();
-    }
-
-    private String deleteNonAlphabeticChars(String string) {
-        return string.replaceAll("[^a-zA-Z]", "");
     }
 }
