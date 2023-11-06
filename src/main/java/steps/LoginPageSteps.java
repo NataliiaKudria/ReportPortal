e95a2d618a4611f04cb0538383fd21c73d3f7417
Nataliia Kudria <nataliia_kudria@epam.com>
2023-11-06 17:16:52 +0200

    replaced previous local link to RP with the new one

--- src/main/java/steps/LoginPageSteps.java
+++ src/main/java/steps/LoginPageSteps.java
@@ -1,17 +1,31 @@
 package steps;
 
 import static drivermanager.CustomWebDriverManager.getDriverInstance;
+import static drivermanager.CustomWebDriverManager.waitForTime;
+import static utils.TimeConstants.LONG_WAIT_MILLISECONDS;
 
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.When;
 import org.jbehave.core.model.ExamplesTable;
 import org.jbehave.core.steps.Parameters;
+import java.util.concurrent.TimeUnit;
 
 public class LoginPageSteps extends BaseSteps {
 
     @Given("User opens the '$url' web site")
     public void openSite(String url) {
         getDriverInstance().get(url);
+    }
+
+    @When("User navigates back")
+    public void navigateBack() {
+        getDriverInstance().navigate().back();
+    }
+
+    @When("On Log In page, user clicks on the 'Login' button")
+    public void clickOnLoginBtn() {
+        loginPage().getSubmitButton().click();
+        waitForTime(LONG_WAIT_MILLISECONDS, TimeUnit.MILLISECONDS);
     }
 
     @When("User logs in with the next data: $table")
@@ -23,5 +37,6 @@ public class LoginPageSteps extends BaseSteps {
         loginPage().getNameInput().sendKeys(login);
         loginPage().getPasswordInput().sendKeys(password);
         loginPage().getSubmitButton().click();
+        waitForTime(LONG_WAIT_MILLISECONDS, TimeUnit.MILLISECONDS);
     }
 }
